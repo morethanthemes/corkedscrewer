b4141d0961b2d5eb1146145622970a167aac27ce
George I. Tsopouridis <gtsopour@gmail.com>
2013-01-09 16:27:36 +0200

    html.tpl.php rearrangement

--- site/sites/all/themes/corkedscrewer/html.tpl.php
+++ site/sites/all/themes/corkedscrewer/html.tpl.php
@@ -39,34 +39,32 @@
  * @see template_preprocess()
  * @see template_preprocess_html()
  * @see template_process()
+ *
+ * @ingroup themeable
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
-    <head profile="<?php print $grddl_profile; ?>">
+<head profile="<?php print $grddl_profile; ?>">
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
 
-        <?php print $head; ?>
-            <title><?php print $head_title; ?></title>  
-        <?php print $styles; ?>
+<?php if (theme_get_setting('responsive_respond','corkedscrewer')): global $base_path; global $base_root; ?>
+<!--[if lt IE 9]>
+<script src="<?php print $base_root . $base_path . path_to_theme() ?>/js/respond.min.js"></script>
+<![endif]-->
+<?php endif; ?>
 
-        <?php if (theme_get_setting('responsive_respond','corkedscrewer')): global $base_path; global $base_root; ?>
-            <!--[if lt IE 9]>
-            <script src="<?php print $base_root . $base_path . path_to_theme() ?>/js/respond.min.js"></script>
-            <![endif]-->
-        <?php endif; ?>
-
-        <?php print $scripts; ?>
-
-    </head>
-
-    <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-        <div id="skip-link">
-            <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-        </div>
-        <?php print $page_top; ?>
-        <?php print $page; ?>
-        <?php print $page_bottom; ?>
-    </body>
-
+<?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<div id="skip-link">
+<a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
+<?php print $page_top; ?>
+<?php print $page; ?>
+<?php print $page_bottom; ?>
+</body>
 </html>
